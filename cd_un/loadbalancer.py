2796b6a7c22b3914aa4cd5f93fdd167bef230b1b
theluismen <luismaocampo28@gmail.com>
2026-04-05 15:22:52 +0000

    feat: dynamic scaling

--- cd_un/loadbalancer.py
+++ cd_un/loadbalancer.py
@@ -7,40 +7,59 @@ import argparse
 Pyro5.config.SERVERTYPE = "thread"
 Pyro5.config.THREADPOOL_SIZE = 50
 
-# URIs de los workers (objectId = "worker")
-worker_uris = [
-    "PYRO:worker@localhost:9001",
-    "PYRO:worker@localhost:9002",
-]
-
 counter = itertools.count()
 thread_local = threading.local()
 
 def get_proxy(uri):
     if not hasattr(thread_local, "proxies"):
-        # Inicializar proxies por hilo
-        thread_local.proxies = {u: Pyro5.api.Proxy(u) for u in worker_uris}
-
         thread_local.proxies = {}
 
-        for uri in worker_uris:
-            proxy = Pyro5.api.Proxy(uri)
-            proxy._pyroBind()          # 🔥 abre conexión aquí            
-            thread_local.proxies[uri] = proxy
+    if uri not in thread_local.proxies:
+        proxy = Pyro5.api.Proxy(uri)
+        proxy._pyroBind()
+        thread_local.proxies[uri] = proxy
 
     return thread_local.proxies[uri]
 
 @Pyro5.api.expose
 class LoadBalancer:
 
+    def __init__(self):
+        self.worker_uris = []
+        self.lock = threading.Lock()
+
+    def register_worker(self, uri):
+        with self.lock:
+            if uri not in self.worker_uris:
+                self.worker_uris.append(uri)
+                print(f"[\033[32m+\033[0m] - Worker + :", f"\033[32m{uri}\033[0m")
+
+    def unregister_worker(self, uri):
+        with self.lock:
+            if uri in self.worker_uris:
+                self.worker_uris.remove(uri)
+                print(f"[\033[31m-\033[0m] - Worker - :", f"\033[31m{uri}\033[0m")
+
+    def get_next_worker(self):
+        with self.lock:
+            if not self.worker_uris:
+                return None
+            return self.worker_uris[next(counter) % len(self.worker_uris)]
+
     def comprar_entrada(self, client_id, request_id):
         print(f"{client_id} -> {request_id}")
-        uri = worker_uris[next(counter) % len(worker_uris)]
+
+        uri = self.get_next_worker()
+        if uri is None:
+            print("No hay workers disponibles")
+            return False
+
         try:
             worker = get_proxy(uri)
             return worker.comprar_entrada(client_id, request_id)
         except Exception as e:
-            print(e)
+            print("Worker caído:", e)
+            self.unregister_worker(uri)  # 🔥 auto-limpieza
             return False
 
 def main():
